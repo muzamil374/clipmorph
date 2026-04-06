2d2b9ee2cc9d45afbcefc4d9058fe378e387f2e9
muzamil374 <stuntboy5959@gmail.com>
2026-04-06 22:08:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+body{
+  background:black;
+  color:white;
+  text-align:center;
+  font-family:sans-serif;
+}
+
+.card{
+  margin-top:50px;
+}
+
+button{
+  padding:10px;
+  background:cyan;
+  border:none;
+}
+
